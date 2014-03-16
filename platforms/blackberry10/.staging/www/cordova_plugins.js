68a74d063f30d44b160b11d9216bf2c58d5740fe
darthgrill <kevin.gagnon@suffolkcountyny.gov>
2014-03-16 11:28:25 -0400

    Trying to get this to work

---
+++ platforms/blackberry10/.staging/www/cordova_plugins.js
@@ -0,0 +1,64 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.blackberry.app/www/client.js",
+        "id": "com.blackberry.app.client",
+        "clobbers": [
+            "blackberry.app"
+        ]
+    },
+    {
+        "file": "plugins/com.blackberry.invoke/www/client.js",
+        "id": "com.blackberry.invoke.client",
+        "merges": [
+            "blackberry.invoke"
+        ]
+    },
+    {
+        "file": "plugins/com.blackberry.invoke/www/btoa.js",
+        "id": "com.blackberry.invoke.btoa",
+        "merges": [
+            "blackberry.invoke.btoa"
+        ]
+    },
+    {
+        "file": "plugins/com.blackberry.invoke.card/www/client.js",
+        "id": "com.blackberry.invoke.card.client",
+        "merges": [
+            "blackberry.invoke.card"
+        ]
+    },
+    {
+        "file": "plugins/com.blackberry.invoke.card/www/btoa.js",
+        "id": "com.blackberry.invoke.card.btoa",
+        "merges": [
+            "blackberry.invoke.card.btoa"
+        ]
+    },
+    {
+        "file": "plugins/com.blackberry.ui.cover/www/client.js",
+        "id": "com.blackberry.ui.cover.client",
+        "clobbers": [
+            "blackberry.ui.cover"
+        ]
+    },
+    {
+        "file": "plugins/com.blackberry.ui.toast/www/client.js",
+        "id": "com.blackberry.ui.toast.client",
+        "clobbers": [
+            "blackberry.ui.toast"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.blackberry.app": "1.0.0",
+    "com.blackberry.invoke": "1.0.0",
+    "com.blackberry.invoke.card": "1.0.0",
+    "com.blackberry.ui.cover": "1.0.0",
+    "com.blackberry.ui.toast": "1.0.0",
+    "com.blackberry.utils": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
